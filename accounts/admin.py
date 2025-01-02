b7ca750e6f371ffe6a037daffa328949fc922585
valluvan-dev <btreerepo.pythonfsd@gmail.com>
2025-01-02 14:38:38 -0800

    first_commit for admin register

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Student, Trainer, Staff
 
-# Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'student_id')
+
+@admin.register(Trainer)
+class TrainerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'trainer_id')
+
+@admin.register(Staff)
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ('user', 'staff_id')
